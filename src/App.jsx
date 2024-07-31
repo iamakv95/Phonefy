7e76934ae14fac8ed6f01e8a659f85dc968a51ef
iamakv21 <iamashishkv95@gmail.com>
2024-07-31 17:26:16 +0530

    project started

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+import { Home, Cart, Error } from "./pages";
+import { MainLayout } from "./layouts";
+
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Error />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Footer, Header } from "../components";
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default MainLayout;
