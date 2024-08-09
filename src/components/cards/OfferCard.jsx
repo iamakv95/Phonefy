28373cc435308d35e084c622511c4b5175187062
iamakv21 <iamashishkv95@gmail.com>
2024-08-09 16:09:48 +0530

    css

--- src/components/cards/OfferCard.jsx
+++ src/components/cards/OfferCard.jsx
@@ -6,7 +6,7 @@ const OfferCard = ({ details,customCSS }) => {
     <div className={` ${customCSS}`} style={{ backgroundImage: `url(${details.imageURL})` }}>
       {details.subTitle && <span className="text-12px font-bold uppercase text-red">{details.subTitle}</span>}
       {details.title && <h4 className="text-28px max-lg:text-24px font-bold capitalize text-black">{details.title}</h4>}
-      {details.descrpt && <p className="text-14px font-semibold capitalize text-black">{details.descrpt}</p>}
+      {details.descrpt && <p className="text-14px max-lg:text-13px max-md:text-12px font-semibold capitalize text-black">{details.descrpt}</p>}
       {details.btnTitle && <Link to={details.href} className="text-12px font-bold uppercase text-black underline underline-offset-4 opacity-90 hover:opacity-100 transition-all duration-500 mt-4">{details.btnTitle}</Link>}
     </div>
   );
